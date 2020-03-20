a53fa8604080bf9077b94f78636c225ad7be79a8
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2020-03-20 10:20:59 -0400

    Changed title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <title>Album example · Bootstrap</title>
+    {{-- <meta name="description" content=""> --}}
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
