9b0ec7f543c308100f9e6b1554f8dc21561e98f1
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2020-03-20 11:19:48 -0400

    changed button text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,112 +31,115 @@
   </head>
   <body>
     <header>
-  <div class="collapse bg-dark" id="navbarHeader">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-8 col-md-7 py-4">
-          <h4 class="text-white">À propos</h4>
-          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut lacinia felis, vitae luctus lectus. Morbi pulvinar id odio id ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed porttitor vulputate leo at faucibus. Donec ut libero aliquam, vestibulum diam non, dignissim neque. Nulla facilisi. Maecenas ullamcorper est nulla. Suspendisse cursus, odio at sollicitudin egestas, augue enim scelerisque sem, eu finibus ligula erat eu ante. </p>
-        </div>
-        <div class="col-sm-4 offset-md-1 py-4">
-          <h4 class="text-white">Contact</h4>
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white">Follow on Twitter</a></li>
-            <li><a href="#" class="text-white">Like on Facebook</a></li>
-            <li><a href="#" class="text-white">Email me</a></li>
-          </ul>
+      <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4 class="text-white">À propos</h4>
+              <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut lacinia felis, vitae luctus lectus. Morbi pulvinar id odio id ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed porttitor vulputate leo at faucibus. Donec ut libero aliquam, vestibulum diam non, dignissim neque. Nulla facilisi. Maecenas ullamcorper est nulla. Suspendisse cursus, odio at sollicitudin egestas, augue enim scelerisque sem, eu finibus ligula erat eu ante. </p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white">Contact</h4>
+              <ul class="list-unstyled">
+                <li><a href="#" class="text-white">Follow on Twitter</a></li>
+                <li><a href="#" class="text-white">Like on Facebook</a></li>
+                <li><a href="#" class="text-white">Email me</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  <div class="navbar navbar-dark bg-dark shadow-sm">
-    <div class="container d-flex justify-content-between">
-      <a href="#" class="navbar-brand d-flex align-items-center">
-        <i class="far fa-compass"></i>&nbsp;
-        <strong>Solution locale</strong>
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    </div>
-  </div>
-</header>
+      <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container d-flex justify-content-between">
+          <a href="#" class="navbar-brand d-flex align-items-center">
+            <i class="far fa-compass"></i>&nbsp;
+            <strong>Solution locale</strong>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
 
-<main role="main">
+    <main role="main">
 
-  <section class="jumbotron text-center">
-    <div class="container">
-      <h1>Allô! C'est moi!</h1>
-      <p class="lead text-muted">Ayons un texte, pour accrocher le monde comme il faut...</p>
-      <p>
-        <a href="#" class="btn btn-primary my-2">Inscrivez votre entreprise</a>
-        <a href="#" class="btn btn-secondary my-2">Un autre bouton!</a>
-      </p>
-    </div>
-  </section>
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1>Allô! C'est moi!</h1>
+          <p class="lead text-muted">Ayons un texte, pour accrocher le monde comme il faut...</p>
+          <p>
+            <a href="#" class="btn btn-primary my-2">Inscrivez une entreprise</a>
+            <a href="#" class="btn btn-secondary my-2">Un autre bouton!</a>
+          </p>
+        </div>
+      </section>
 
-  <div class="album py-5 bg-light">
-    <div class="container">
+      <div class="album py-5 bg-light">
+        <div class="container">
 
-      <div class="row">
-        <div class="col-md-12 text-center mb-5 h5">
-          <a href="/" class="badge badge-info">Tout le Québec <span class="badge badge-light">{{ App\Place::count() }}</span></a>
-          @foreach(App\Region::all() as $region)
-          <a href="{{ route("public.index-region", ['region' => $region->slug]) }}" class="badge badge-info">{{ $region->name }} <span class="badge badge-light">{{ $region->places->count() }}</span></a>
+          <div class="row">
+            <div class="col-md-12 text-center mb-5 h5">
+              <a href="/" class="badge badge-info">Tout le Québec <span class="badge badge-light">{{ App\Place::count() }}</span></a>
+              @foreach(App\Region::all() as $region)
+              <a href="{{ route("public.index-region", ['region' => $region->slug]) }}" class="badge badge-info">{{ $region->name }} <span class="badge badge-light">{{ $region->places->count() }}</span></a>
+              @endforeach
+            </div>
+          </div>
+
+          @foreach($places as $place)
+            <div class="col-md-8 offset-md-2">
+              <div class="card mb-4 shadow-sm">
+                {{-- <img src="/images/solutionlocale-placeholder.png" class="bd-placeholder-img card-img-top" alt="Solution locale"> --}}
+                <div class="card-body">
+                  <h4 class="card-title text-center">{{ $place->name }}</h4>
+                  <h5 class="text-center"><i class="fas fa-map-marker-alt"></i> {{ $place->region->name }}, {{ $place->subRegion }}</h5>
+
+                  <div class="container mt-3">
+                    <div class="row">
+                      <div class="col">
+                        <p class="text-center">
+                          <i class="fas fa-shopping-cart"></i> Méthode d'acquisition
+                        </p>
+                      </div>
+                      <div class="col">
+                        <p class="text-center">
+                          <i class="fas fa-tags"></i> Catégories
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  {{-- @if($place->delivery->isNotEmpty())<p class="card-text"><i class="fas fa-shopping-cart"></i> {{ $place->delivery->implode('name', ', ') }}</p>@endif --}}
+
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      @auth()
+                      <a class="btn btn-sm btn-outline-secondary" href="{{ route('places.edit', ['place' => $place->slug]) }}">Edit</a>
+                      @endauth
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           @endforeach
         </div>
       </div>
 
-      @foreach($places as $place)
-        <div class="col-md-8 offset-md-2">
-          <div class="card mb-4 shadow-sm">
-            {{-- <img src="/images/solutionlocale-placeholder.png" class="bd-placeholder-img card-img-top" alt="Solution locale"> --}}
-            <div class="card-body">
-              <h4 class="card-title text-center">{{ $place->name }}</h4>
-              <h5 class="text-center"><i class="fas fa-map-marker-alt"></i> {{ $place->region->name }}, {{ $place->subRegion }}</h5>
+    </main>
 
-              <div class="container mt-3">
-                <div class="row">
-                  <div class="col">
-                    <p class="text-center">
-                      <i class="fas fa-shopping-cart"></i> Méthode d'acquisition
-                    </p>
-                  </div>
-                  <div class="col">
-                    <p class="text-center">
-                      <i class="fas fa-tags"></i> Catégories
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              {{-- @if($place->delivery->isNotEmpty())<p class="card-text"><i class="fas fa-shopping-cart"></i> {{ $place->delivery->implode('name', ', ') }}</p>@endif --}}
-
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  @auth()
-                  <a class="btn btn-sm btn-outline-secondary" href="{{ route('places.edit', ['place' => $place->slug]) }}">Edit</a>
-                  @endauth
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      @endforeach
-    </div>
-  </div>
-
-</main>
-
-<footer class="text-muted">
-  <div class="container">
-    <p class="float-right">
-      <a href="#">Retour en haut</a>
-    </p>
-    <p>Du texte et du copyright &copy; à nous pis toute.</p>
-  </div>
-</footer>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script></body>
+    <footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+          <a href="#">Retour en haut</a>
+        </p>
+        <p>Du texte et du copyright &copy; à nous pis toute.</p>
+      </div>
+    </footer>
+    
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    
+    <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+</body>
 </html>
