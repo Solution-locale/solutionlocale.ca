1e3b9fedaaae7180cc09964c4af01d6ce989f48b
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2020-03-19 22:17:41 -0400

    Correct display of multiple places

--- resources/views/byCategory.blade.php
+++ resources/views/byCategory.blade.php
@@ -10,25 +10,27 @@
             </div>
             @endif
 
-            <div class="card-deck">
+            <div class="row row-cols-1 row-cols-md-2">
                 @foreach($places as $place)
-                <div class="card">
-                    {{-- <img src="..." class="card-img-top" alt="..."> --}}
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $place->name }}</h5>
-                        
-                        <p><address><i class="fas fa-map-marker-alt"></i> {{ $place->address }}, {{ $place->city }} {{ $place->postalCode }}</address></p>
-                        @if($place->phoneNumber !== null)<p><i class="fas fa-phone"></i> {{ $place->phoneNumber }}</p>@endif
-                        @if($place->additionnalPhoneNumber !== null)<p><i class="fas fa-phone"></i> {{ $place->additionnalPhoneNumber }}</p>@endif
-                        @if($place->email !== null)<p><i class="fas fa-at"></i> {{ $place->email }}</p>@endif
-                        @if($place->url !== null)<p><i class="fas fa-desktop"></i> {{ $place->url }}</p>@endif
-                        <p class="card-text"><i class="fas fa-truck"></i> {{ $place->deliveryZone }}</p>
-                        <p class="card-text"><i class="fas fa-info-circle"></i> {{ $place->instructions }}</p>
+                    <div class="col mb-4">
+                        <div class="card">
+                            {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $place->name }}</h5>
+                                
+                                <p><address><i class="fas fa-map-marker-alt"></i> {{ $place->address }}, {{ $place->city }} {{ $place->postalCode }}</address></p>
+                                @if($place->phoneNumber !== null)<p><i class="fas fa-phone"></i> {{ $place->phoneNumber }}</p>@endif
+                                @if($place->additionnalPhoneNumber !== null)<p><i class="fas fa-phone"></i> {{ $place->additionnalPhoneNumber }}</p>@endif
+                                @if($place->email !== null)<p><i class="fas fa-at"></i> {{ $place->email }}</p>@endif
+                                @if($place->url !== null)<p><i class="fas fa-desktop"></i> {{ $place->url }}</p>@endif
+                                <p class="card-text"><i class="fas fa-truck"></i> {{ $place->delivery->implode('name', ', ') }}</p>
+                                <p class="card-text"><i class="fas fa-info-circle"></i> {{ $place->instructions }}</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">{{ $place->region->name }}, {{ $place->subRegion }}</small>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-footer">
-                        <small class="text-muted">{{ $place->region }}</small>
-                    </div>
-                </div>
                 @endforeach
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,25 +8,27 @@
               Vous naviguez dans toutes les inscriptions sur le site. Vous pouvez trier avec les options du menu ci-haut.
             </div>
 
-            <div class="card-deck">
+            <div class="row row-cols-1 row-cols-md-2">
                 @foreach(App\Place::all() as $place)
-                <div class="card">
-                    {{-- <img src="..." class="card-img-top" alt="..."> --}}
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $place->name }}</h5>
-                        
-                        <p><address><i class="fas fa-map-marker-alt"></i> {{ $place->address }}, {{ $place->city }} {{ $place->postalCode }}</address></p>
-                        @if($place->phoneNumber !== null)<p><i class="fas fa-phone"></i> {{ $place->phoneNumber }}</p>@endif
-                        @if($place->additionnalPhoneNumber !== null)<p><i class="fas fa-phone"></i> {{ $place->additionnalPhoneNumber }}</p>@endif
-                        @if($place->email !== null)<p><i class="fas fa-at"></i> {{ $place->email }}</p>@endif
-                        @if($place->url !== null)<p><i class="fas fa-desktop"></i> {{ $place->url }}</p>@endif
-                        <p class="card-text"><i class="fas fa-truck"></i> {{ $place->delivery->implode('name', ', ') }}</p>
-                        <p class="card-text"><i class="fas fa-info-circle"></i> {{ $place->instructions }}</p>
+                    <div class="col mb-4">
+                        <div class="card">
+                            {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $place->name }}</h5>
+                                
+                                <p><address><i class="fas fa-map-marker-alt"></i> {{ $place->address }}, {{ $place->city }} {{ $place->postalCode }}</address></p>
+                                @if($place->phoneNumber !== null)<p><i class="fas fa-phone"></i> {{ $place->phoneNumber }}</p>@endif
+                                @if($place->additionnalPhoneNumber !== null)<p><i class="fas fa-phone"></i> {{ $place->additionnalPhoneNumber }}</p>@endif
+                                @if($place->email !== null)<p><i class="fas fa-at"></i> {{ $place->email }}</p>@endif
+                                @if($place->url !== null)<p><i class="fas fa-desktop"></i> {{ $place->url }}</p>@endif
+                                <p class="card-text"><i class="fas fa-truck"></i> {{ $place->delivery->implode('name', ', ') }}</p>
+                                <p class="card-text"><i class="fas fa-info-circle"></i> {{ $place->instructions }}</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">{{ $place->region->name }}, {{ $place->subRegion }}</small>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-footer">
-                        <small class="text-muted">{{ $place->region->name }}, {{ $place->subRegion }}</small>
-                    </div>
-                </div>
                 @endforeach
             </div>
         </div>
