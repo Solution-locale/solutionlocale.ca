b2215b55969e740fb4db7d7500e5a721f2f7d2d5
Rachid Zuco (PC - miniTour - Win7) <r.zuco@yahoo.fr>
2020-03-26 19:25:15 -0400

    added factories

---
+++ database/factories/DeliveryTypeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\DeliveryType;
+use Faker\Generator as Faker;
+
+$factory->define(DeliveryType::class, function (Faker $faker) {
+    return [
+        'name'  =>  $faker->words()
+    ];
+});
---
+++ database/factories/RegionFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Region;
+use Faker\Generator as Faker;
+
+$factory->define(Region::class, function (Faker $faker) {
+    return [
+        'name'  =>  $faker->name,
+        'slug'  =>  $faker->slug
+    ];
+});
