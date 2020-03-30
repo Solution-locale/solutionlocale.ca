63925aa3939ba07a774669f70be4656f9b98a612
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2020-03-30 19:11:40 -0400

    Clarified placeholder

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -131,7 +131,7 @@ Nouvelle entreprise - {{ config('app.name', '') }}
 
                     <div class="col-md-9">
                         <input type="text" class="form-control" id="inputAddress" placeholder="1234 rue de l'achat local" name="address[line1]" required="" value="{{ old('address.line1') }}">
-                        <input type="text" class="form-control mt-2" id="inputAddress2" placeholder="Appartement, no. de porte, etc. (optionnel) " name="address[line2]" value="{{ old('address.line2') }}">
+                        <input type="text" class="form-control mt-2" id="inputAddress2" placeholder="Appartement, etc. (optionnel) " name="address[line2]" value="{{ old('address.line2') }}">
                     </div>
                 </div>
 
