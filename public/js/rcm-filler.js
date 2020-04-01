9d0b70b64ec5954683d6f8d64901911de6ab4738
Martin Carpentier <mc@voyagesarabais.com>
2020-04-01 10:06:11 -0400

    Better RCM filling process

--- public/js/rcm-filler.js
+++ public/js/rcm-filler.js
@@ -1,22 +1,41 @@
 $(document).ready(function() {
-    $("[name='region_id']").on("change", function() {
-        // Getting the region's ID
+    var $region = $("[name='region_id']");
+    var $rcm = $("[name='rcm_id']");
+    var rcms = [];
+
+    // Event called when user change region entry.
+    $region.on("change", function() {
         var $me = $(this);
+
         var id = $me.find('option:selected').val();
         if (!id) { return; }
+        id = isNaN(id) ? null : parseInt(id);
 
-        // Showing loading message on RCM drop down.
-        var $rcm = $("[name='rcm_id']");
-        $rcm.html('<option>Chargement en cours...</option>');
-
-        // Sending request.
-        $.getJSON('/mrc/json/'+id, function(rcms) {
-            $rcm.html('');
-
-            // Populating RCM drop down with results.
-            $.each(rcms, function(idx, rcm) {
-                $rcm.append('<option value="' + rcm.id + '">' + rcm.name + '</option>');
-            });
-        });
+        $rcm.html('');
+        for (var i = 0; i < rcms.length; i++) {
+            if (rcms[i].region_id == id) {
+                $rcm.append('<option data-region="' + rcms[i].region_id + '" value="' + rcms[i].rcm_id + '">' + rcms[i].rcm_name + '</option>');
+            }
+        }
     });
+
+    // Keeping all the RCM information into memory.
+    $rcm.find('option').each(function() {
+        var region = $(this).data("region");
+        rcms.push({ region_id: region, rcm_id: $(this).val(), rcm_name: $(this).text().trim() });
+    });
+
+    // In case we arrive on the page and the region list is already filled.
+    if (!isNaN($region.find('option:selected').val())) {
+        var selectedRegion = $region.find('option:selected').val();
+        $rcm.find('option').each(function() {
+            var rcmRegion = $(this).data("region");
+            if (selectedRegion != rcmRegion) {
+                $(this).remove();
+            }
+        });
+    } else {
+        $region.trigger('change');
+    }
+
 });
